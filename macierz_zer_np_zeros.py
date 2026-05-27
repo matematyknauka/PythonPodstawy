3af725cc5ea774a54eb248421c7ff4640d0b2809
matematyknauka <matematyknauka@gmail.com>
2026-05-27 15:39:22 +0200

    W pliku tablica.py dopisano dtype. Dodano pliki macierz_zer_np_zeros.py oraz zwykla_petla_for_ma_slowko_range.py.

---
+++ macierz_zer_np_zeros.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+macierz = np.zeros((10, 5), dtype = np.uint8)
+
+print(macierz)
--- tablica.py
+++ tablica.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 # Tworzenie tablicy z listy
-tablica = np.array([1, 2, 3, 4, 5])
+tablica = np.array([1, 2, 3, 4, 5], dtype = np.uint8)
 print(tablica)
 print(type(tablica)) # Zobaczysz <class 'numpy.ndarray'>
