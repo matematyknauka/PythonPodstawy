e7bbf918f576f8f7b2c402972ca204d699cb113c
matematyknauka <matematyknauka@gmail.com>
2026-06-18 16:46:46 +0200

    Ćwiczenie Odczytanie klatek i zapis każdej do nowego filmu.

--- WycinanieTlaSredniaOdchylenie/film.py
+++ WycinanieTlaSredniaOdchylenie/film.py
@@ -1,13 +1,32 @@
 import cv2
 
+from algorytm import zakres_min, zakres_max
+k = 2 # k można modyfikować.
+tlo_bgr = cv2.imread("tlo.jpg")
+tlo_wiersze, tlo_kolumny, _ = tlo_bgr.shape
+tlo_hsv = cv2.cvtColor(tlo_bgr, cv2.COLOR_BGR2HSV)
+h_min = zakres_min(tlo_hsv, tlo_wiersze, tlo_kolumny, 0, k)
+h_max = zakres_max(tlo_hsv, tlo_wiersze, tlo_kolumny, 0, k)
+s_min = zakres_min(tlo_hsv, tlo_wiersze, tlo_kolumny, 1, k)
+s_max = zakres_max(tlo_hsv, tlo_wiersze, tlo_kolumny, 1, k)
+v_min = zakres_min(tlo_hsv, tlo_wiersze, tlo_kolumny, 2, k)
+v_max = zakres_max(tlo_hsv, tlo_wiersze, tlo_kolumny, 2, k)
+
+
+
+
 film = cv2.VideoCapture("film_testowy.mp4")
+film_klatki_na_sekunde = film.get(cv2.CAP_PROP_FPS)
+film_szerokosc_klatki = int(film.get(cv2.CAP_PROP_FRAME_WIDTH))
+film_wysokosc_klatki = int(film.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+film_rezultat = cv2.VideoWriter("wynik.mp4", fourcc, film_klatki_na_sekunde, (film_szerokosc_klatki, film_wysokosc_klatki))
 
 sukces, klatka = film.read()
-i = 1
-
 while sukces:
-    print(f"klatka {i}")
-    i = i + 1
+    film_rezultat.write(klatka)
     sukces, klatka = film.read()
 
 film.release()
+film_rezultat.release()
