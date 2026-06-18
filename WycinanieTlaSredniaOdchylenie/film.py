0df1f443646ca5349e29a8e5063640f9180a6ee6
matematyknauka <matematyknauka@gmail.com>
2026-06-18 17:23:00 +0200

    Gotowy skrypt wycinania tła działający. Dla pewności jeszcze testować go.

--- WycinanieTlaSredniaOdchylenie/film.py
+++ WycinanieTlaSredniaOdchylenie/film.py
@@ -1,7 +1,7 @@
 import cv2
 
-from algorytm import zakres_min, zakres_max
-k = 2 # k można modyfikować.
+from algorytm import zakres_min, zakres_max, wytnij_tlo
+k = 20 #k można modyfikować.
 tlo_bgr = cv2.imread("tlo.jpg")
 tlo_wiersze, tlo_kolumny, _ = tlo_bgr.shape
 tlo_hsv = cv2.cvtColor(tlo_bgr, cv2.COLOR_BGR2HSV)
@@ -23,10 +23,12 @@ film_wysokosc_klatki = int(film.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 film_rezultat = cv2.VideoWriter("wynik.mp4", fourcc, film_klatki_na_sekunde, (film_szerokosc_klatki, film_wysokosc_klatki))
 
-sukces, klatka = film.read()
+sukces, klatka_bgr = film.read()
 while sukces:
-    film_rezultat.write(klatka)
-    sukces, klatka = film.read()
+    klatka_hsv = cv2.cvtColor(klatka_bgr, cv2.COLOR_BGR2HSV)
+    wiersze_klatki, kolumny_klatki, _ = klatka_bgr.shape
+    film_rezultat.write(wytnij_tlo(klatka_bgr, klatka_hsv, wiersze_klatki, kolumny_klatki, h_min, h_max, s_min, s_max, v_min, v_max))
+    sukces, klatka_bgr = film.read()
 
 film.release()
 film_rezultat.release()
